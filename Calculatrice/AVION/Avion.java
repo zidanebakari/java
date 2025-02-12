0adab40367ed47f9fa3bde4b9f5283583ba1a28b
zidane <bakarizidane25@gmail.com>
2025-02-12 16:41:18 +0100

    test

---
+++ Calculatrice/AVION/Avion.java
@@ -0,0 +1,9 @@
+package Calculatrice.AVION;
+
+public class Avion implements Volant {
+    @Override
+    public void deplacer (){  
+        system.out.printin("l'avion vole dans les air")
+
+    }
+}
