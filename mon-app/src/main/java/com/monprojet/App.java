512b62384805ac0ad4065e5b8958d672ea474420
zidane <bakarizidane25@gmail.com>
2025-02-18 14:33:13 +0100

    message

---
+++ mon-app/src/main/java/com/monprojet/App.java
@@ -0,0 +1,13 @@
+package com.monprojet;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
