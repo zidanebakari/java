a4c8bd8007721bd65afcae6ece377cf9b22171c4
zidane <bakarizidane25@gmail.com>
2025-02-19 15:48:02 +0100

    message

--- sql-app/src/main/java/com/monprojet/App.java
+++ sql-app/src/main/java/com/monprojet/App.java
@@ -1,13 +1,33 @@
 package com.monprojet;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class App {
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/mabase";
+        String username = "root";
+        String password = "";
+
+        // Utilisation du try-with-resources pour gérer la fermeture automatique des
+        // ressources
+        try (Connection conn = DriverManager.getConnection(url, username, password);
+                Statement stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery("SELECT id, nom, email FROM utilisateur")) {
+
+            System.out.println("Liste des utilisateur :");
+            // Parcours du ResultSet
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String nom = rs.getString("nom");
+                String email = rs.getString("email");
+                System.out.println("ID : " + id + ", Nom : " + nom + ", Email : " + email);
+            }
+        } catch (SQLException e) {
+            System.err.println("Erreur lors de l'exécution de la requête SELECT : " + e.getMessage());
+        }
     }
 }
